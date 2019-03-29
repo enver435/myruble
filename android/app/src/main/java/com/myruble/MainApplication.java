f8d7e6e02c96260ea2cc53d941f110ef6256bee6
Your Name <you@example.com>
2019-03-29 16:31:56 +0400

    added firebase crashlytics

--- android/app/src/main/java/com/myruble/MainApplication.java
+++ android/app/src/main/java/com/myruble/MainApplication.java
@@ -8,6 +8,7 @@ import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
             new RNFirebaseAdMobPackage(),
             new RNFirebaseMessagingPackage(),
             new RNFirebaseNotificationsPackage(),
+            new RNFirebaseCrashlyticsPackage(),
             new RNDeviceInfo(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage()
